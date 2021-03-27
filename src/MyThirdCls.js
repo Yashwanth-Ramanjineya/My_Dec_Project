4b7392c28d9c4c9a0a489752c6f5c78371f371e4
sumilkumar157 <ksunilkumar157gmail.com>
2021-03-27 11:48:08 +0530

    thirdcls added

---
+++ src/MyThirdCls.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+function MyThirdCls() {
+  return <h1>This is "git" changes</h1>;
+}
+export default MyThirdCls;
--- src/index.js
+++ src/index.js
@@ -1,48 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Route, BrowserRouter as Router, Link, Switch} from 'react-router-dom';
-import MyApp from './MyApp';
-import MyFirstClassComponent from './MyFirstClassComponent';
-import MyFirstComponent from './MyFirstCompoent';
-import NotFound from './NotFound';
-import { createStore } from 'redux';
-import myReducer from './myReducer';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Route, BrowserRouter as Router, Link, Switch } from "react-router-dom";
+import MyApp from "./MyApp";
+import MyFirstClassComponent from "./MyFirstClassComponent";
+import MyFirstComponent from "./MyFirstCompoent";
+import NotFound from "./NotFound";
+import { createStore } from "redux";
+import myReducer from "./myReducer";
+import { Provider } from "react-redux";
+import MyThirdCls from "./MyThirdCls";
 
 const myStore = createStore(myReducer);
-
 
 const routing = (
   <Router>
     <div>
       <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/myapp">MY App</Link></li>
-        <li><Link to="/myfirstclass">MY First Class</Link></li>
-        <li><Link to="/mySecondclass">My Second Class</Link></li> 
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/myapp">MY App</Link>
+        </li>
+        <li>
+          <Link to="/myfirstclass">MY First Class</Link>
+        </li>
+        <li>
+          <Link to="/mySecondclass">My Second Class</Link>
+        </li>
+        <li>
+          <Link to="/MyThirdCls">MyThirdCls</Link>
+        </li>
       </ul>
       <Switch>
-      <Route exact path="/" component={App} />
-      <Route exact path="/myapp" component={MyApp} />
-      <Route exact path="/myfirstclass" component={MyFirstClassComponent}/>
-      <Route exact path="/mySecondclass" component={MyFirstComponent} />
-      <Route component={NotFound} />
+        <Route exact path="/" component={App} />
+        <Route exact path="/myapp" component={MyApp} />
+        <Route exact path="/myfirstclass" component={MyFirstClassComponent} />
+        <Route exact path="/mySecondclass" component={MyFirstComponent} />
+        <Route exact path="/MyThirdCls" component={MyThirdCls} />
+        <Route component={NotFound} />
       </Switch>
     </div>
   </Router>
-)
+);
 
 ReactDOM.render(
   <Provider store={myStore}>
-    <React.StrictMode>
-    {routing}
-  </React.StrictMode>
-  </Provider>
-  ,
-  document.getElementById('root')
+    <React.StrictMode>{routing}</React.StrictMode>
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
